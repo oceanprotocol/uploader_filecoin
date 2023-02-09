64e03d329d2b7fbf8c49d4e57ad81ee37a850f48
xlassix <awhy14539@gmail.com>
2023-02-09 22:56:42 +0100

    rebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-  parserOptions: {
-    sourceType: "module",
-  },
-  parser: "babel-eslint",
-  env: {
-    node: true,
-  },
-  extends: [
-    "standard",
-    "prettier",
-    "prettier/standard",
-    "plugin:jest/recommended",
-  ],
-  plugins: ["prettier", "jest"],
-  rules: {
-    "promise/catch-or-return": "error",
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-        semi: false,
-      },
+    parserOptions: {
+        sourceType: 'module',
+    },
+    parser: 'babel-eslint',
+    env: {
+        node: true,
+    },
+    extends: [
+        'standard',
+        'prettier',
+        'prettier/standard',
+        'plugin:jest/recommended',
+        'airbnb-base',
+        'plugin:prettier/recommended',
     ],
-  },
-};
+    plugins: ['prettier', 'jest'],
+    rules: {
+        'promise/catch-or-return': 'error',
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                semi: false,
+            },
+        ],
+    },
+}
