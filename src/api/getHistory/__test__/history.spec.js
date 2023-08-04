0d7542db93e7681e581853fb496296258f8ad592
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-08-04 16:58:44 +0300

    Refactored test.

--- src/api/getHistory/__test__/history.spec.js
+++ src/api/getHistory/__test__/history.spec.js
@@ -17,13 +17,46 @@ describe('quota', () => {
   describe('Test', () => {
 
     test('get history', async () => {
-      const nonce = Date.now();
+      let requestQuotaresponse = await request(app)
+        .post(`/getQuote`)
+        .send({
+          type: 'filecoin',
+          files: [
+            {
+              length: 0,
+            },
+          ],
+          payment: {
+            chainId: 80001,
+            tokenAddress: '0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2',
+          },
+          duration: 4353545453,
+          userAddress: wallet.address,
+        });
+      let nonce = Date.now();
+      
+      let message = utils.sha256(utils.toUtf8Bytes(requestQuotaresponse.body.quoteId + nonce.toString()))
+      // Sign the original message directly
+      let signature = await wallet.signMessage(message)
+
+      let response = await request(app)
+        .post(`/upload`)
+        .send({
+          quoteId: requestQuotaresponse.body.quoteId,
+          nonce,
+          signature,
+          files: ['ipfs://QmaiauHSgTDMy2NtLbsygL3iKmLXBqHf39SBA1nAQFSSey'],
+        });
+      expect(typeof response.body).toBe('object');
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toMatch(/success/i);
+      nonce = Date.now();
       const quoteId = '';
-      const message = utils.sha256(utils.toUtf8Bytes(quoteId + nonce.toString()))
+      message = utils.sha256(utils.toUtf8Bytes(quoteId + nonce.toString()))
     
       // Sign the original message directly
-      const signature = await wallet.signMessage(message)
-      let response = await request(app).get(
+      signature = await wallet.signMessage(message)
+      response = await request(app).get(
         `/getHistory?userAddress=${
           wallet.address
         }&nonce=${nonce}&signature=${signature}`
