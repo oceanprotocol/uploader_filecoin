18eed711bc3a0066c72326a6e41bda07e78e28b2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-08-31 13:35:33 +0300

    Adding a test for the paginated getHistory endpoint

--- src/api/getHistory/__test__/history.spec.js
+++ src/api/getHistory/__test__/history.spec.js
@@ -15,42 +15,41 @@ describe('quota', () => {
   });
 
   describe('Test', () => {
-
     test('get history', async () => {
       const nonce = Date.now();
       const quoteId = '';
-      const message = utils.sha256(utils.toUtf8Bytes(quoteId + nonce.toString()))
-    
+      const message = utils.sha256(
+        utils.toUtf8Bytes(quoteId + nonce.toString())
+      );
+
       // Sign the original message directly
-      const signature = await wallet.signMessage(message)
+      const signature = await wallet.signMessage(message);
       let response = await request(app).get(
-        `/getHistory?userAddress=${
-          wallet.address
-        }&nonce=${nonce}&signature=${signature}`
+        `/getHistory?userAddress=${wallet.address}&nonce=${nonce}&signature=${signature}`
       );
       expect(typeof response.body).toBe('object');
       expect(response.statusCode).toBe(200);
-      expect(response.body.data.length).toBe(0); 
+      expect(response.body.data.length).toBe(0);
     });
 
-    test("fails if method not allowed", async () => {
+    test('fails if method not allowed', async () => {
       const nonce = Date.now();
       const quoteId = '';
-      const message = utils.sha256(utils.toUtf8Bytes(quoteId + nonce.toString()))
-    
+      const message = utils.sha256(
+        utils.toUtf8Bytes(quoteId + nonce.toString())
+      );
+
       // Sign the original message directly
-      const signature = await wallet.signMessage(message)
+      const signature = await wallet.signMessage(message);
       let response = await request(app).post(
-        `/getHistory?userAddress=${
-        wallet.address
-      }&nonce=${nonce}&signature=${signature}`
+        `/getHistory?userAddress=${wallet.address}&nonce=${nonce}&signature=${signature}`
       );
       expect(typeof response.body).toBe('object');
       expect(response.statusCode).toBe(405);
       expect(response.body.message).toBe('Method Not allowed');
     });
 
-    test("fails if signature not provided", async () => {
+    test('fails if signature not provided', async () => {
       const signature = '';
       let response = await request(app).get(
         `/getHistory?userAddress=${
@@ -62,5 +61,26 @@ describe('quota', () => {
       expect(response.body.message).toBe('Invalid signature');
     });
 
+    test('get history with pagination', async () => {
+      const nonce = Date.now();
+      const quoteId = '';
+      const message = utils.sha256(
+        utils.toUtf8Bytes(quoteId + nonce.toString())
+      );
+
+      // Sign the original message directly
+      const signature = await wallet.signMessage(message);
+
+      const page = 2;
+      const pageSize = 5;
+
+      let response = await request(app).get(
+        `/getHistory?userAddress=${wallet.address}&nonce=${nonce}&signature=${signature}&page=${page}&pageSize=${pageSize}`
+      );
+
+      expect(typeof response.body).toBe('object');
+      expect(response.statusCode).toBe(200);
+      expect(response.body.data.length).toBeLessThanOrEqual(5);
+    });
   });
 });
