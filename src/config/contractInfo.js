fee76d49f9eb7f2d92e43ec559f7b438839ee045
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-09-04 13:04:23 +0300

    Fixing the token names

--- src/config/contractInfo.js
+++ src/config/contractInfo.js
@@ -19,8 +19,8 @@ export default isFilecoinProduction
   : {
       80001: {
         currency: {
-          USDT: '0x742DfA5Aa70a8212857966D491D67B09Ce7D6ec7',
-          USDC: '0x21C561e551638401b937b03fE5a0a0652B99B7DD',
+          USDC: '0x742DfA5Aa70a8212857966D491D67B09Ce7D6ec7',
+          USDT: '0x21C561e551638401b937b03fE5a0a0652B99B7DD',
           TUSD: '0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2',
         },
         rpc: process.env.MUMBAI_RPC,
