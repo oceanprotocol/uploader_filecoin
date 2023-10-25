f40a2d8e20aa5081ad168201b5d5b59b7ec988c3
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-10-25 15:02:57 +0300

    Updating schema validation

--- src/api/upload/upload.validator.js
+++ src/api/upload/upload.validator.js
@@ -1,5 +1,5 @@
-import Joi from 'joi';
-import { SchemaValidator } from '../../util/validator';
+const Joi = require('joi');
+const { SchemaValidator } = require('../../util/validator');
 
 const createOptions = {
   abortEarly: false, // include all errors
@@ -7,21 +7,25 @@ const createOptions = {
   stripUnknown: true, // remove unknown props
 };
 
+const fileSchema = Joi.object({
+  content_type: Joi.string().optional(), // optional field
+  ipfs_uri: Joi.string()
+    .pattern(/^ipfs:\/\//i)
+    .required(), // required field with pattern check
+}).required();
+
 const uploadInputSchema = Joi.object({
   quoteId: Joi.string().required().min(0),
   nonce: Joi.number().required().min(0),
   signature: Joi.string().required(),
-  files: Joi.array()
-    .min(1)
-    .required()
-    .items(
-      Joi.string()
-        .pattern(/^ipfs:\/\//i)
-        .required()
-    ),
+  files: Joi.array().min(1).required().items(fileSchema), // using the updated fileSchema
 });
 
-export const uploadInputSchemaValidator = SchemaValidator(
+const uploadInputSchemaValidator = SchemaValidator(
   uploadInputSchema,
   createOptions
 );
+
+module.exports = {
+  uploadInputSchemaValidator,
+};
